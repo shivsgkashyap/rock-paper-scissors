6f2e33e0547496747a4ddabc63a5e7839ed7123c
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-16 18:35:01 +0000

    fix image paths

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import github from "../images/github.png";
 
 export default function Footer() {
   return (
     <footer class="footer">
       <p>Copyright © {new Date().getFullYear()} Shiv Kashyap</p>
       <a href="https://github.com/shivsgkashyap" target="_blank">
-        <img src="src/images/github.png" alt="Github Logo" />
+        <img src={github} alt="Github Logo" />
       </a>
     </footer>
   );
