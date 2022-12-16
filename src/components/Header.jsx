b25aa2f13c2bd1639e4cb0e4ad35b970869bba6a
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-16 18:38:41 +0000

    Fixed all image paths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import rps from "../images/rps.jpg";
 
 export default function Header() {
   return (
     <header className="navbar-items">
-      <img src="./src/images/rps.jpg" alt="Rock,Paper,Scissors Logo" />
-      <img src="../src/images/rps.jpg" alt="Rock,Paper,Scissors Logo" />
-      <img src="../../src/images/rps.jpg" alt="Rock,Paper,Scissors Logo" />
+      <img src={rps} alt="Rock,Paper,Scissors Logo" />
       <h1 className="title">ROCK, PAPER, SCISSORS</h1>
     </header>
   );
