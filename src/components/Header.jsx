843e565492e34ac3f4ec7c432728539899973879
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-16 17:47:35 +0000

    Fixing image paths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="navbar-items">
-      <img src="./images/rps.jpg" alt="Rock,Paper,Scissors Logo" />
+      <img src="main/src/images/github.png" alt="Rock,Paper,Scissors Logo" />
       <h1 className="title">ROCK, PAPER, SCISSORS</h1>
     </header>
   );
